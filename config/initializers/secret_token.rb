ed353bbe81d80c3c68347aca7598cc66442cf1d7
Paris Sinclair <paris@rubypanther.com>
2012-01-11 18:24:20 -0800

    initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MtiExample::Application.config.secret_token = 'ad3038224209282aa196864ef260b77a182634564d5aaecbf3dab9bffa899be986f46356a75181d07b57318b8a4d47e79c51920186d8b6546d3ba95f28c6f2a1'
